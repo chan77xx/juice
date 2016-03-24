e4e9f531e30cabc00138f24c3f277766b090bc8d
chan77xx <chan77xx@gmail.com>
2016-03-25 00:54:32 +0900

    Update juice.js

--- juice/juice.js
+++ juice/juice.js
@@ -3,9 +3,22 @@
  * Author	: chan77xx
  * Contact  : chan77xx@gmail.com
  *******************************************************************************/
-var JDataset = function() {
- 	
+var JComponent = function() {
+    this.container = document.createElement('span');
 };
-JDataset.prototype.setColumn = function(name, value) {
+/**
+ * JUnitComponent
+ */
+var JUnitComponent = function() {};
+JUnitComponent.prototype = new JCompoment();
 
-}
+/**
+ * JCompositeComponent
+ */
+var JCompositeComponent = function() {};
+JCompositeComponent.prototype = new JCompoment();
+
+var JEdit = function() {};
+JEdit.prototype = new JUnitComponent();
+
+
