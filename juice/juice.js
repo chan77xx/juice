b2b7db2ba177a78d34cb2c6269eb9a79ce4793c1
chan77xx <chan77xx@gmail.com>
2016-03-25 00:10:37 +0900

    fda

--- juice/juice.js
+++ juice/juice.js
@@ -1,1 +1,8 @@
-test
+/*******************************************************************************
+ * JUICE (Javascript User Interface Component Engine
+ * Author	: chan77xx
+ * Contact  : chan77xx@gmail.com
+ *******************************************************************************/
+var JDataset = function() {
+ 	
+};
